9b06ae96213cb88a013cdf312d940d12b83b8974
Dharani Sivashanmugam <155830580+Dharani-S93@users.noreply.github.com>
2024-12-15 23:04:20 +0530

    Time: 1 ms (99.80%), Space: 62.1 MB (13.76%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -3,18 +3,13 @@ class Solution {
         int j=0;
         int i=0;
         int diff=0;
-        while(j<prices.length)
-        {
-            if(prices[i]>=prices[j])
-            {
-                i=j;
-            }
-            else
-            {
+        while(j<prices.length){
+            if(prices[i]>=prices[j]){
+                i=j; }
+            else {
                 diff=Math.max(diff,prices[j]-prices[i]);
             }
             j++;
         }
         return diff;
-    }
-}
+    }}
