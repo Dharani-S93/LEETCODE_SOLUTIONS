7b97f0105dc90c251edde2b48b31841184283923
Dharani Sivashanmugam <155830580+Dharani-S93@users.noreply.github.com>
2025-02-17 19:32:17 +0530

    Time: 3 ms (69.13%), Space: 41.4 MB (85.91%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.java
@@ -2,26 +2,21 @@ class Solution {
     private int count = 0;
 
     public int numTilePossibilities(String tiles) {
-        int[] freq = new int[26]; // Array to store frequency of letters
-
-        // Count frequency of each character in tiles
+        int[] freq = new int[26]; 
         for (char ch : tiles.toCharArray()) {
             freq[ch - 'A']++;
         }
-
         backtrack(freq);
         return count; 
     }
-
     private void backtrack(int[] freq) {
         for (int i = 0; i < 26; i++) {
-            if (freq[i] > 0) { // If this letter is available
-                count++; // Count this sequence
-                freq[i]--; // Use this letter
+            if (freq[i] > 0) { 
+                count++;
+                freq[i]--; 
+                backtrack(freq); 
                 
-                backtrack(freq); // Recursively find other sequences
-                
-                freq[i]++; // Backtrack (restore frequency)
+                freq[i]++; 
             }
         }
     }
