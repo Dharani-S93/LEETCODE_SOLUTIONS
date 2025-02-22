8629b031084040b12ff61e2280babbcc3e76f65a
Dharani Sivashanmugam <155830580+Dharani-S93@users.noreply.github.com>
2025-02-22 09:33:07 +0530

    Time: 2 ms (87.91%), Space: 44.6 MB (90.11%) - LeetHub

---
+++ 1093-recover-a-tree-from-preorder-traversal/1093-recover-a-tree-from-preorder-traversal.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int val) { this.val = val; }
+}
+
+class Solution {
+    int index = 0;
+
+    public TreeNode recoverFromPreorder(String traversal) {
+        return helper(traversal, 0);
+    }
+
+    private TreeNode helper(String s, int depth) {
+        int numDashes = 0;
+        while (index < s.length() && s.charAt(index) == '-') {
+            numDashes++;
+            index++;
+        }
+
+        if (numDashes != depth) {
+            index -= numDashes;
+            return null;
+        }
+
+        int numStart = index;
+        while (index < s.length() && Character.isDigit(s.charAt(index))) {
+            index++;
+        }
+        
+        int value = Integer.parseInt(s.substring(numStart, index));
+        TreeNode node = new TreeNode(value);
+        node.left = helper(s, depth + 1);
+        node.right = helper(s, depth + 1);
+        
+        return node;
+    }
+}
